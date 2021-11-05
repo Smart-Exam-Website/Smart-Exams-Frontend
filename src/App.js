3c35a0052ce10aa6c8904e09208cbcb901f92bd1
Hossam Sherif <husam287@hotmail.com>
2021-11-05 16:07:31 +0200

    fix router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import Footer from './Components/Footer/Footer';
@@ -7,31 +6,28 @@ import Login from "./Components/Registration/Login/Login";
 import SignupInquiry from "./Components/Registration/Register/RegisterInquiry";
 import SignupInstructor from "./Components/Registration/Register/RegisterInstructor/RegisterInstructor";
 import SignupStudent from "./Components/Registration/Register/RegisterStudent/RegisterStudent";
-import { store } from "./redux/store";
 import Home from './Views/Home/Home';
 import InstructorProfile from "./Views/Profiles/Instructor-Profile/InstructorProfile";
 import StudentProfile from "./Views/Profiles/Student-Profile/StudentProfile";
 
 function App() {
   return (
-    <Provider store={store}>
+    <BrowserRouter basename="/Smart-Exams-Frontend">
       <Header />
 
-      <BrowserRouter basename="/Smart-Exams-Frontend">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={SignupInquiry} />
-          <Route exact path="/register-student" component={SignupStudent} />
-          <Route exact path="/register-instructor" component={SignupInstructor} />
-          <Route exact path="/profile/student" component={StudentProfile} />
-          <Route exact path="/profile/instructor" component={InstructorProfile} />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={SignupInquiry} />
+        <Route exact path="/register-student" component={SignupStudent} />
+        <Route exact path="/register-instructor" component={SignupInstructor} />
+        <Route exact path="/profile/student" component={StudentProfile} />
+        <Route exact path="/profile/instructor" component={InstructorProfile} />
+      </Switch>
 
 
       <Footer />
-    </Provider>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
