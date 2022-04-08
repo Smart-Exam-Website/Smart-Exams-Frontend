74a39bb9ae2d580389275227666614ab7a591b3d
Hossam Sherif <husam287@hotmail.com>
2022-04-08 19:37:54 +0200

    fix my results

--- src/Views/Student/ResultDetails/index.js
+++ src/Views/Student/ResultDetails/index.js
@@ -2,7 +2,9 @@ import { Grid } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { StudentServices } from '../../../apis/Services/StudentService'
+import EssayAnswer from '../../../Components/AnsweredQuestion/EssayAnswer'
 import McqAnswer from '../../../Components/AnsweredQuestion/McqAnswer'
+import { QuestionTypes } from '../../../constants/QuestionTypes'
 import HandleErrors from '../../../hooks/handleErrors'
 
 const ResultDetails = () => {
@@ -24,16 +26,27 @@ const ResultDetails = () => {
             <div className="row mt-5">
                 <div className="col-12">
                     {studentExamResult?.map(item =>
-                        <div className='my-2'>
-                            {(item?.question?.type === 'mcq') ?
+                        <div key={item.question_id} className='my-2'>
+                            {(item?.question?.type === QuestionTypes.MCQ) ?
                                 <McqAnswer
-                                    key={item.question_id}
                                     studentAnswer={{ id: item?.option_id, value: item?.studentAnswer }}
                                     questionText={item?.question?.questionText}
-                                    choices={item?.question?.answers}
                                     studentMark={item?.questionMark}
                                     questionMark={item?.totalQuestionMark}
                                     isMarked={item?.isMarked}
+                                    choices={item?.question?.answers}
+                                />
+                                :
+                                null
+                            }
+                            {(item?.question?.type === QuestionTypes.ESSAY) ?
+                                <EssayAnswer
+                                    studentAnswer={{ id: item?.option_id, value: item?.studentAnswer }}
+                                    questionText={item?.question?.questionText}
+                                    studentMark={item?.questionMark}
+                                    questionMark={item?.totalQuestionMark}
+                                    isMarked={item?.isMarked}
+                                    correctAnswer={item?.question?.answers[0]}
                                 />
                                 :
                                 null
